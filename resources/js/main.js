88d9107c31e01c5fe05219fbb164b5f649905392
stanleypapaya <stanleychenxingyu@gmail.com>
2018-11-18 10:58:30 +0800

    重置自动轮播位置修复

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,7 @@
 var allButtons = $('#buttons > button')
 
+
+// 1.添加按钮点击事件
 for (let i = 0; i < allButtons.length; i++) {
     $(allButtons[i]).on('click', function (e) {
         var index = $(e.currentTarget).index()
@@ -8,22 +10,16 @@ for (let i = 0; i < allButtons.length; i++) {
         $('#images').css({
             'transform': 'translateX(' + p + 'px)'
         })
+        n = i
     })
 }
 
-var n = 0
-var numberOfImages = allButtons.length
-var timer = function () {
-    id = setInterval(() => {
-        n += 1
-        let m = n % numberOfImages
-        $(allButtons[m]).trigger('click')
-            .addClass('active').siblings().removeClass('active')
-    }, 2500)
-}
-
+// 2.实现自动轮播
+let n = 0
+let numberOfImages = allButtons.length
 timer()
 
+// 3.鼠标进出控制
 $('.imgContainer').mouseenter(function () {
     clearInterval(id)
 })
@@ -31,3 +27,13 @@ $('.imgContainer').mouseenter(function () {
 $('.imgContainer').mouseleave(function () {
     timer()
 })
+
+// 4. 函数
+function timer() {
+    id = setInterval(() => {
+        n += 1
+        let m = n % numberOfImages
+        $(allButtons[m]).trigger('click')
+            .addClass('active').siblings().removeClass('active')
+    }, 3000)
+}
